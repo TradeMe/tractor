59a85a94a8fe79749fce6a2cd7095a17b0097197
Craig Spence <craigspence0@gmail.com>
2015-02-24 13:38:59 +1300

    Just some build tidy up stuff.

--- packages/tractor/build/bundle.js
+++ packages/tractor/build/bundle.js
@@ -1,23 +1,25 @@
 'use strict';
 
+// Config:
 var config      = require('./config.js');
+var packageJSON = require('../package.json')
 
+// Dependencies:
 var gulp        = require('gulp');
 var eslint      = require('gulp-eslint');
 var browserify  = require('browserify');
 var source      = require('vinyl-source-stream');
 var buffer      = require('vinyl-buffer');
 var ngAnnotate  = require('gulp-ng-annotate');
-var uglify      = require('gulp-uglify');
-var sourcemaps  = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 
+// Errors:
 var error = require('./utilities/error-handler');
 
 gulp.task('bundle', function() {
     gulp.src(['src/**/*.js', 'server/**/*.js'])
     .pipe(eslint())
-    .pipe(eslint.format())
+    .pipe(eslint.format());
 
     return browserify({
         entries: [config.appDir + 'app.js']
@@ -25,20 +27,14 @@ gulp.task('bundle', function() {
     .transform('brfs')
     .bundle()
     .on('error', error)
-    .pipe(source(getBundleName() + '.js'))
+    .pipe(source(packageJSON.name + '.bundle.js'))
     .pipe(buffer())
     .pipe(ngAnnotate({
         add: true,
         single_quotes: true
     }))
-    // .pipe(sourcemaps.init({ loadMaps: true }))
-    // .pipe(uglify())
-    // .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.dest))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(browserSync.reload({
+        stream: true
+    }));
 });
-
-function getBundleName () {
-    var app = require('../package.json');
-    return app.name + '.' + 'min';
-};
--- packages/tractor/build/watch.js
+++ packages/tractor/build/watch.js
@@ -1,7 +1,9 @@
 'use strict';
 
+// Config:
 var config = require('./config');
 
+// Dependencies:
 var gulp   = require('gulp');
 var reload = require('./reload');
 
