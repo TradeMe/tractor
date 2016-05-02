8da41d13c29cece71fffe03e3e92b96448e9dd89
Galileo1 <vgaur@TradeMe.local>
2016-05-02 14:58:53 +1200

    incorporating review comments

--- server/cli/init/base-file-sources/hooks.js
+++ server/cli/init/base-file-sources/hooks.js
@@ -1,41 +1,38 @@
 'use strict';
 
 /* eslint-disable no-var, prefer-arrow-callback */
-var path = require('path');
-var cucumber = require('cucumber');
 var constants = require('../../../constants')
-var outputDir = path.join(__dirname, '../', constants.REPORT_DIR)
-var fs = require('fs');
+var cucumber = require('cucumber');
+var path = require('path');
+var Promise = require('bluebird');
+
+var fs = Promise.promisifyAll(require('fs'));
  /* eslint-disable new-cap */
-var jsonFormatter = cucumber.Listener.JsonFormatter();
+var jsonReportFormatter = cucumber.Listener.JsonFormatter();
+var outputDir = path.join(__dirname, '../', constants.REPORT_DIR)
 
-
-var tractorReportHook = function () {
-
-    var createHtmlReport = function (sourceJson) {
-        var CucumberHtmlReport = require('cucumber-html-report');
-        var report = new CucumberHtmlReport({
-            source: sourceJson, // source json
-            dest: outputDir, // target directory (will create if not exists)
-            name: 'tractor_report.html'  // report file name (will be index.html if not exists)
-        });
-        report.createReport();
-    };
-
-    jsonFormatter.log = function (string) {
-        var cucumberReport = path.join(outputDir, 'cucumber_report.json');
-        fs.writeFile(cucumberReport, string, function (err) {
-            if (err) {
-                console.log('EError:Failed to save test results to json file.');
-                console.log(err);
-            } else {
-                createHtmlReport(cucumberReport);
-            }
-        });
-    };
-
-    this.registerListener(jsonFormatter);
-
+jsonReportFormatter.log = function (string) {
+    var cucumberReport = path.join(outputDir, 'cucumber_report.json');
+    fs.writeFile(cucumberReport, string, function (err) {
+        if (err) {
+            console.log('EError:Failed to save test results to json file.');
+            console.log(err);
+        } else {
+            createHtmlReport(cucumberReport);
+        }
+    });
 };
 
-module.exports = tractorReportHook;
+function createHtmlReport (sourceJson) {
+    var CucumberHtmlReport = require('cucumber-html-report');
+    var report = new CucumberHtmlReport({
+        source: sourceJson, // source json
+        dest: outputDir, // target directory (will create if not exists)
+        name: 'tractor_report.html'  // report file name (will be index.html if not exists)
+    });
+    report.createReport();
+}
+
+module.exports = function () {
+    this.registerListener(jsonReportFormatter);
+};
