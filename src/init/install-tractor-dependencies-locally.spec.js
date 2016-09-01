3dd2543f1a668b39fb5cc60840dcd01e3857ac79
Craig Spence <craigspence0@gmail.com>
2016-09-01 12:01:05 +1200

    Bump server

--- src/init/install-tractor-dependencies-locally.spec.js
+++ src/init/install-tractor-dependencies-locally.spec.js
@@ -67,7 +67,7 @@ describe('cli/init: install-tractor-dependencies-locally:', () => {
             expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact httpbackend@1.2.1');
             expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact protractor@2.5.1');
             expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact tractor-plugin-browser@0.1.0-beta.1');
-            expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact tractor-plugin-loader@0.1.0-beta.5');
+            expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact tractor-plugin-loader@0.1.0-beta.6');
         })
         .finally(() => {
             childProcess.exec.restore();
@@ -99,7 +99,7 @@ describe('cli/init: install-tractor-dependencies-locally:', () => {
             expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact httpbackend@1.2.1');
             expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact protractor@2.5.1');
             expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact tractor-plugin-browser@0.1.0-beta.1');
-            expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact tractor-plugin-loader@0.1.0-beta.5');
+            expect(childProcess.execAsync).to.have.been.calledWith('npm install --save-dev --save-exact tractor-plugin-loader@0.1.0-beta.6');
         })
         .finally(() => {
             childProcess.exec.restore();
@@ -140,7 +140,7 @@ describe('cli/init: install-tractor-dependencies-locally:', () => {
         sinon.stub(childProcess, 'exec').returns({
             stdout: {
                 on: (event, callback) => {
-                    callback('bluebird@2.10.2 chai@2.3.0 chai-as-promised@5.1.0 cucumber@0.7.0 cucumber-html-report@0.2.5 httpbackend@1.2.1 protractor@2.5.1 tractor-plugin-browser@0.1.0-beta.1 tractor-plugin-loader@0.1.0-beta.5');
+                    callback('bluebird@2.10.2 chai@2.3.0 chai-as-promised@5.1.0 cucumber@0.7.0 cucumber-html-report@0.2.5 httpbackend@1.2.1 protractor@2.5.1 tractor-plugin-browser@0.1.0-beta.1 tractor-plugin-loader@0.1.0-beta.6');
                 }
             }
         });
