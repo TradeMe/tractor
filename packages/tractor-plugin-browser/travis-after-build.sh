370be6328055bccdf3c23f94d7ce8c8574d45fd6
Craig Spence <craigspence0@gmail.com>
2017-01-26 18:18:59 +1300

    (cosmetic :lipstick:) - Adding some logging to after-build script

--- packages/tractor-plugin-browser/travis-after-build.sh
+++ packages/tractor-plugin-browser/travis-after-build.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST_BRANCH != *"greenkeeper"* ]]; then
-	# Not a GreenKeeper Pull Request, aborting
-	exit 0
+    echo "This is not a greenkeeper PR, nothing to do 😎"
+    exit 0
 fi
 
+echo "Cloning repo... 💿"
 git clone "https://"$PUSH_TOKEN"@github.com/"$TRAVIS_REPO_SLUG".git" repo
+echo "Done 👍"
 cd repo
 
+echo "Switching to branch $TRAVIS_PULL_REQUEST_BRANCH... 🌴"
 git checkout $TRAVIS_PULL_REQUEST_BRANCH
+echo "Done 👍"
 
 # See if commit message includes "update"
 git log --name-status HEAD^..HEAD | grep "update" || exit 0
 
-# Run yarn to create/update lockfile
+echo "(Creat/updat)ing lockfile... 🔒"
 yarn
+echo "Done 👍"
 
-# Commit and push yarn.lock file
+echo "Committing and pushing yarn.lock... 🖐"
 git config --global user.email "no@no.com"
 git config --global user.name "Travis CI"
 git config --global push.default simple
 
 git add yarn.lock
-git commit -m "chore: update yarn.lock"
+git commit -m "(dependencies ￼￼📦) - update yarn.lock"
 git push
+echo "Done 👍"
