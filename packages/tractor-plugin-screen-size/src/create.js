d88d311c42c26b03085676b0e0f57e3a894ab3f5
Craig Spence <craigspence0@gmail.com>
2017-02-06 22:28:16 +1300

    (bug fix :bug:) - fixing path reference

--- packages/tractor-plugin-screen-size/src/create.js
+++ packages/tractor-plugin-screen-size/src/create.js
@@ -1,5 +1,5 @@
 // Dependencies:
-import { ScreenSize } from './screen-size';
+import { ScreenSize } from './screen-size/screen-size';
 
 export default function create (browser, config) {
     return new ScreenSize(browser, config);
