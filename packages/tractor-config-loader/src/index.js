9bdc5b8eca6bfc8a19afa4c919ea843b038f120a
Craig Spence <craigspence0@gmail.com>
2017-09-01 19:00:28 +1200

    Merge pull request #28 from phenomnomnominal/wip
    (pull request 🦄) - this branch ended up not being much at all

--- packages/tractor-config-loader/src/index.js
+++ packages/tractor-config-loader/src/index.js
@@ -31,20 +31,22 @@ export function loadConfig () {
     }
     config = defaults(config, DEFAULT_CONFIG);
 
-    sortTags(config);
+    config.tags = sortTags(config.tags);
+
     return config;
 }
 
-function sortTags (config) {
+function sortTags (tags) {
     // Remove empty tag
-    if (config.tags.includes(EMPTY_TAG)) {
-        let index = config.tags.indexOf(EMPTY_TAG);
-        config.tags.splice(index, 1);
+    if (tags.includes(EMPTY_TAG)) {
+        let index = tags.indexOf(EMPTY_TAG);
+        tags.splice(index, 1);
     }
 
     // Add @s
-    config.tags = config.tags.map(tag => tag.startsWith('@') ? tag : `@${tag}`);
+    tags = tags.map(tag => tag.startsWith('@') ? tag : `@${tag}`);
 
     // Add empty tag to start
-    config.tags.unshift(EMPTY_TAG);
+    tags.unshift(EMPTY_TAG);
+    return tags;
 }
