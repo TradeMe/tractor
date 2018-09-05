143aa73c89f37f2c2753f336f57d176622e03ee5
Craig Spence <craigspence0@gmail.com>
2018-09-05 15:12:01 +1200

    fix(ui :lipstick:) - determine modification by diffing the generated JS rather than the AST

--- packages/tractor-ui/src/app/Core/Services/ASTCompareService.js
+++ packages/tractor-ui/src/app/Core/Services/ASTCompareService.js
@@ -4,44 +4,37 @@
 var Core = require('../Core');
 
 // Dependencies:
-var deepEqual = require('deep-equal')
-var esquery = require('esquery');
+var Diff = require('text-diff');
+var escodegen = require('escodegen');
 
 var ASTCompareService = function () {
+    var diff = new Diff();
     return { compare };
 
     function compare (a, b) {
-        // There are some version mismatches between
-        // different estools. We modify the ASTs to ensure
-        // they have the same expected properties:
-        downgradeProgram(a);
-        downgradeProgram(b);
-        downgradeFunctionExpressions(a);
-        downgradeFunctionExpressions(b);
-        downgradeStringLiterals(a);
-        downgradeStringLiterals(b);
+        if (a == null || b == null) {
+            return false;
+        }
+        
+        var jsA = escodegen.generate(a);
+        var jsB = escodegen.generate(b);
+        var difference = diff.main(jsA, jsB);
+        var isTheSame = diff.levenshtein(difference) === 0;
 
-        return deepEqual(a, b);
-    }
+        /* eslint-disable */
+        if (!isTheSame && window.__tractor__ && window.__tractor__.debug) {
+            diff.cleanupSemantic(difference);
+            const pre = document.createElement('pre');
+            const code = document.createElement('code');
+            code.innerHTML = diff.prettyHtml(difference);
+            pre.appendChild(code);
+            document.body.appendChild(pre);
+            const app = document.querySelector('body > [ui-view]');
+            app.parentNode.removeChild(app);
+        }
+        /* eslint-enable */
 
-    function downgradeProgram (ast) {
-        esquery(ast, 'Program').forEach(program => {
-            delete program.sourceType;
-        });
-    }
-
-    function downgradeFunctionExpressions (ast) {
-        esquery(ast, 'FunctionExpression').forEach(functionExpression => {
-            delete functionExpression.async;
-            delete functionExpression.defaults;
-            delete functionExpression.rest;
-        });
-    }
-
-    function downgradeStringLiterals (ast) {
-        esquery(ast, 'Literal[raw]').forEach(stringLiteral => {
-            delete stringLiteral.raw;
-        });
+        return isTheSame;
     }
 };
 
