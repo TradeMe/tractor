13160570c791a988b5c8e93cfd9360c998b587ba
Craig Spence <craigspence0@gmail.com>
2019-01-18 17:48:23 +1300

    chore(tests :rotating_light:) - make sure output directories exist before tests run

--- plugins/mocha-specs/tractor/protractor.conf.js
+++ plugins/mocha-specs/tractor/protractor.conf.js
@@ -1,11 +1,5 @@
 // Dependencies:
-const { promisify } = require('bluebird');
-const mkdir = promisify(require('fs').mkdir);
-const rimraf = promisify(require('rimraf'));
 const { tractor } = require('@tractor/core');
-
-// Constants:
-const TEST_DIRECTORY = './test';
 
 exports.config = tractor('../tractor.conf.js').plugin({
     allScriptsTimeout: 11000,
@@ -16,13 +10,5 @@ exports.config = tractor('../tractor.conf.js').plugin({
     mochaOpts: {
         timeout: 30000
     },
-    plugins: [{
-        inline: {
-            onPrepare () {
-                return rimraf(TEST_DIRECTORY)
-                .then(() => mkdir(TEST_DIRECTORY));
-            }
-        }
-    }],
     SELENIUM_PROMISE_MANAGER: false
 });
