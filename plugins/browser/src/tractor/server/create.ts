010c321dd30fce6e45486fa3a06e5e90a9cef8f9
Craig Spence <craigspence0@gmail.com>
2019-02-09 15:24:45 +1300

    chore(browser :earth_africa:) - just rearranging some stuff

--- plugins/browser/src/tractor/server/create.ts
+++ plugins/browser/src/tractor/server/create.ts
@@ -2,5 +2,7 @@
 import { INJECTION, TractorDIFunc } from '@tractor/dependency-injection';
 import { TractorBrowser } from '../../tractor-browser';
 
-export const create: TractorDIFunc<TractorBrowser> = (browser: TractorBrowser): TractorBrowser => browser;
-create[INJECTION] = ['browser'];
+export function create (browser: TractorBrowser): TractorBrowser {
+    return browser;
+}
+(create as TractorDIFunc<TractorBrowser>)[INJECTION] = ['browser'];
