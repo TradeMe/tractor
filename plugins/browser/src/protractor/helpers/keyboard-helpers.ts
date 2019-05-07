9d7565fa66929a2b1a01ed16d8595e49bdc39654
gtopeto <49378767+gtopeto@users.noreply.github.com>
2019-05-08 11:42:05 +1200

    feat(browser 🌏) - add browser.pasteText

--- plugins/browser/src/protractor/helpers/keyboard-helpers.ts
+++ plugins/browser/src/protractor/helpers/keyboard-helpers.ts
@@ -1,7 +1,24 @@
 // Dependencies:
-import { Key, ProtractorBrowser } from 'protractor';
+import { ElementFinder, Key, ProtractorBrowser } from 'protractor';
+import { TractorBrowser } from '../../tractor-browser';
 
-export function addKeyboardHelpers (browser: ProtractorBrowser): void {
+export function addKeyboardHelpers (browser: TractorBrowser | ProtractorBrowser): void {
+    browser.focusNext = async function (): Promise<void> {
+        return sendKeyAction(Key.TAB);
+    };
+
+    browser.focusPrevious = async function (): Promise<void> {
+        return sendKeyAction(Key.SHIFT, Key.TAB);
+    };
+
+    browser.pasteText = async function (element: ElementFinder, text: string): Promise<void> {
+        await element.click();
+        await (browser as ProtractorBrowser).executeScript(`arguments[0].setAttribute('value', arguments[1]); arguments[0].value = arguments[1]`, element, text);
+        await sendKeyAction(Key.DOWN);
+        await element.sendKeys(' ');
+        await (browser as TractorBrowser).sendDeleteKey();
+    };
+
     browser.sendDeleteKey = async function (): Promise<void> {
         return sendKeyAction(Key.DELETE);
     };
@@ -14,15 +31,7 @@ export function addKeyboardHelpers (browser: ProtractorBrowser): void {
         return sendKeyAction(Key.SPACE);
     };
 
-    browser.focusNext = async function (): Promise<void> {
-        return sendKeyAction(Key.TAB);
-    };
-
-    browser.focusPrevious = async function (): Promise<void> {
-        return sendKeyAction(Key.SHIFT, Key.TAB);
-    };
-
     async function sendKeyAction (...keys: Array<string>): Promise<void> {
-        return browser.actions().sendKeys(Key.chord(...keys)).perform() as Promise<void>;
+        return (browser as ProtractorBrowser).actions().sendKeys(Key.chord(...keys)).perform() as Promise<void>;
     }
 }
